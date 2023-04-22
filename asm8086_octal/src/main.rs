6091aae039435504c60b5a43e98ff4da8cffcff6
mihsamusev <mih.samusev@gmail.com>
2023-04-22 21:56:56 +0200

    refactor

--- asm8086_octal/src/main.rs
+++ asm8086_octal/src/main.rs
@@ -12,7 +12,7 @@ fn read_bytes(filename: &str) -> io::Result<Vec<u8>> {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-enum HalfRegister {
+enum ByteRegister {
     AL,
     CL,
     DL,
@@ -21,18 +21,18 @@ enum HalfRegister {
     CH,
     DH,
     BH,
-    Unresolved
+    Unread
 }
 
-use HalfRegister::*;
-impl HalfRegister {
+use ByteRegister::*;
+impl ByteRegister {
     const VALUES: [Self; 8] = [AL, CL, DL, BL, AH, CH, DH, BH];
     fn from_r(r: u8) -> Self {
-        HalfRegister::VALUES[r as usize]
+        ByteRegister::VALUES[r as usize]
     }
 }
 #[derive(Debug, Clone, Copy, PartialEq)]
-enum FullRegister {
+enum WordRegister {
     AX,
     CX,
     DX,
@@ -41,25 +41,68 @@ enum FullRegister {
     BP,
     SI,
     DI,
-    Unresolved
+    Unread
 }
 
-use FullRegister::*;
-impl FullRegister {
+use WordRegister::*;
+impl WordRegister {
     const VALUES: [Self; 8] = [AX, CX, DX, BX, SP, BP, SI, DI];
     fn from_r(r: u8) -> Self {
-        FullRegister::VALUES[r as usize]
+        WordRegister::VALUES[r as usize]
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Pointer {
+    BX_SI(u16),
+    BX_DI(u16),
+    BP_SI(u16),
+    BP_DI(u16),
+    SI(u16),
+    DI(u16),
+    Direct(u16),
+    BX(u16),
+    Unread
+}
 
+impl Pointer {
+    fn from_r(r: u8, value: u16) -> Self {
+       match r {
+        0 => Pointer::BX_SI(value),
+        1 => Pointer::BX_DI(value),
+        2 => Pointer::BP_SI(value),
+        3 => Pointer::BP_DI(value),
+        4 => Pointer::SI(value),
+        5 => Pointer::DI(value),
+        6 => Pointer::Direct(value),
+        7 => Pointer::BX(value),
+        _ => Pointer::Unread
+       }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Address {
+    ByteRegister(ByteRegister),
+    WordRegister(WordRegister),
+    Pointer(Pointer),
+    ByteRegisterUnread,
+    WordRegisterUnread,
+    PointerUnread,
+}
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum Operand {
-    Rb(HalfRegister),
-    Rw(FullRegister),
-    Eb(HalfRegister), // effective address byte
-    Ew(FullRegister), // effective word
+    Rb(ByteRegister),
+    Rw(WordRegister),
+    Eb(ByteRegister), // effective address byte
+    Ew(WordRegister), // effective word
+    Db(u8),
+    Dw(u16),
+    Dc(i8),
+    DnUnread,
+    DcUnread,
+    DwUnread,
     SR, // segment register
 }
 
@@ -67,10 +110,10 @@ impl Display for Operand {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         use Operand::*;
         match self {
-            Rb(reg) if (*reg) != HalfRegister::Unresolved => write!(f, "{reg:?}"),
-            Eb(reg) if (*reg) != HalfRegister::Unresolved => write!(f, "{reg:?}"),
-            Rw(reg) if (*reg) != FullRegister:: Unresolved => write!(f, "{reg:?}"),
-            Ew(reg) if (*reg) != FullRegister:: Unresolved => write!(f, "{reg:?}"),
+            Rb(reg) if (*reg) != ByteRegister::Unread => write!(f, "{reg:?}"),
+            Eb(reg) if (*reg) != ByteRegister::Unread => write!(f, "{reg:?}"),
+            Rw(reg) if (*reg) != WordRegister:: Unread => write!(f, "{reg:?}"),
+            Ew(reg) if (*reg) != WordRegister:: Unread => write!(f, "{reg:?}"),
             _ => write!(f, "{self:?}")
         }
     }
@@ -86,12 +129,16 @@ enum Asm8086 {
 fn opcode_to_instruction(opcode_byte: u8) -> Asm8086 {
     use Operand::*;
     match opcode_byte {
-        0o210 => Asm8086::Mov(Eb(HalfRegister::Unresolved), Rb(HalfRegister::Unresolved)),
-        0o211 => Asm8086::Mov(Ew(FullRegister::Unresolved), Rw(FullRegister::Unresolved)),
-        0o212 => Asm8086::Mov(Rb(HalfRegister::Unresolved), Eb(HalfRegister::Unresolved)),
-        0o213 => Asm8086::Mov(Rw(FullRegister::Unresolved), Ew(FullRegister::Unresolved)),
-        0o214 => Asm8086::Mov(Ew(FullRegister::Unresolved), SR),
-        0o216 => Asm8086::Mov(SR, Ew(FullRegister::Unresolved)),
+        0o210 => Asm8086::Mov(Eb(ByteRegister::Unread), Rb(ByteRegister::Unread)),
+        0o211 => Asm8086::Mov(Ew(WordRegister::Unread), Rw(WordRegister::Unread)),
+        0o212 => Asm8086::Mov(Rb(ByteRegister::Unread), Eb(ByteRegister::Unread)),
+        0o213 => Asm8086::Mov(Rw(WordRegister::Unread), Ew(WordRegister::Unread)),
+        0o214 => Asm8086::Mov(Ew(WordRegister::Unread), SR),
+        0o216 => Asm8086::Mov(SR, Ew(WordRegister::Unread)),
+        0o261 => Asm8086::Mov(Rb(ByteRegister::CL), DcUnread),
+        0o265 => Asm8086::Mov(Rb(ByteRegister::CH), DcUnread),
+        0o271 => Asm8086::Mov(Rw(WordRegister::CX), DwUnread),
+        0o272 => Asm8086::Mov(Rw(WordRegister::DX), DwUnread),
         _  => Asm8086::Unknown
     }
 }
@@ -105,38 +152,72 @@ fn resolve_mov_operands(byte: u8) -> (u8, u8, u8) {
 
 fn resolve_register_mode(register_operand: Operand, r: u8) -> Operand {
     match (register_operand, r) {
-        (Operand::Rb(HalfRegister::Unresolved), 0..=7) => Operand::Rb(HalfRegister::from_r(r)),
-        (Operand::Eb(HalfRegister::Unresolved), 0..=7) => Operand::Eb(HalfRegister::from_r(r)),
-        (Operand::Rw(FullRegister::Unresolved), 0..=7) => Operand::Rw(FullRegister::from_r(r)),
-        (Operand::Ew(FullRegister::Unresolved), 0..=7) => Operand::Ew(FullRegister::from_r(r)),
+        (Operand::Rb(ByteRegister::Unread), 0..=7) => Operand::Rb(ByteRegister::from_r(r)),
+        (Operand::Eb(ByteRegister::Unread), 0..=7) => Operand::Eb(ByteRegister::from_r(r)),
+        (Operand::Rw(WordRegister::Unread), 0..=7) => Operand::Rw(WordRegister::from_r(r)),
+        (Operand::Ew(WordRegister::Unread), 0..=7) => Operand::Ew(WordRegister::from_r(r)),
         _ => register_operand
     }
 }
 
 
+
 fn resolve_operands_address(operand: Operand, x: u8, r_or_s: u8, m: u8) -> Operand {
     use Operand::*;
-    match operand {
-        Rb(_) | Rw(_) => resolve_register_mode(operand, r_or_s),
-        Eb(_) | Ew(_) => resolve_register_mode(operand, m),
+    match (operand, x, m) {
+        (Rb(_) | Rw(_), _, _) => resolve_register_mode(operand, r_or_s),
+        (Eb(_) | Ew(_), 3, _) => resolve_register_mode(operand, m),
         _ => operand
     }
 }
-fn main() -> io::Result<()>{
+
+fn to_u16(low_bit: u8, high_bit: u8) -> u16 {
+    ((high_bit as u16) << 8) | (low_bit as u16)
+}
+
+fn main() -> Result<(), String>{
      //let mut instructions = Vec::new();
-    let mut bytes = read_bytes("multiple_mov.bin")?;
+    let mut bytes = read_bytes("listing_0039_more_movs.bin").expect("cant");
     bytes.as_mut_slice().reverse();
     while let Some(first_bit) =  bytes.pop() {
-        match opcode_to_instruction(first_bit) {
+        let opcode =opcode_to_instruction(first_bit);  
+        match opcode {
+            Asm8086::Mov(reg, value) if value == Operand::DcUnread => {
+                let value_bit = bytes.pop().ok_or("could not finish parsing")?;
+                let value = value_bit as i8;
+                println!("[{first_bit:#o}][{value_bit:#o}] = MOV {reg}, {value}")
+            },
+            Asm8086::Mov(reg, value) if value == Operand::DwUnread => {
+                let low_bit = bytes.pop().ok_or("could not finish parsing")?;
+                let high_bit = bytes.pop().ok_or("could not finish parsing")?;
+                let value= to_u16(low_bit, high_bit);
+                println!("[{first_bit:#o}][{low_bit:#o}][{high_bit:#o}] = MOV {reg}, {value}")
+            },
             Asm8086::Mov(dest, src) => { 
-                print!("{first_bit:#o} is MOV {dest:?}, {src:?}\t");
-                if let Some(second_bit) = bytes.pop() {
-                    let (x, r_or_s, m)= resolve_mov_operands(second_bit);
-                    let src = resolve_operands_address(src, x, r_or_s, m);
-                    let dest = resolve_operands_address(dest, x, r_or_s, m);
-                    println!("MOV {dest} {src}")
-                } else {
-                    panic!("Could not finish parsing...")
+                let second_bit = bytes.pop().ok_or("could not finish parsing")?;
+                let (x, r_or_s, m)= resolve_mov_operands(second_bit);
+                match x {
+                    0 => {
+                        let value = if r_or_s == 6 { second_bit} else {0};
+                        Pointer::from_r(r_or_s, value as u16);
+                        println!("[{first_bit:#o}][{second_bit:#o}] = MOV {dest}, {src}");
+                    },
+                    1 => {
+                        let low_bit = bytes.pop().ok_or("could not finish parsing")?;
+                        println!("[{first_bit:#o}][{second_bit:#o}][{low_bit:#o}] = MOV {dest}, {src}");
+                    },
+                    2 => {
+                        let low_bit = bytes.pop().ok_or("could not finish parsing")?;
+                        let high_bit = bytes.pop().ok_or("could not finish parsing")?;
+                        let value= to_u16(low_bit, high_bit);
+                        println!("[{first_bit:#o}][{second_bit:#o}][{low_bit:#0}][{high_bit:#o}] = MOV {dest}, {src}");
+                   },
+                    3 => {
+                        let src = resolve_operands_address(src, x, r_or_s, m);
+                        let dest = resolve_operands_address(dest, x, r_or_s, m);
+                        println!("[{first_bit:#o}][{second_bit:#o}] = MOV {dest}, {src}");
+                    },
+                    _ => {}
                 }
             },
             Asm8086::Unknown => println!("unable to parse opcode bit {first_bit:#o}")
@@ -144,4 +225,36 @@ fn main() -> io::Result<()>{
     } 
     Ok(())
 }
+
+
+//  ; Register-to-register
+//   20   │ mov si, bx
+//   21   │ mov dh, al
+//   22   │ 
+//   23   │ ; 8-bit immediate-to-register
+//   24   │ mov cl, 12
+//   25   │ mov ch, -12
+//   26   │ 
+//   27   │ ; 16-bit immediate-to-register
+//   28   │ mov cx, 12
+//   29   │ mov cx, -12
+//   30   │ mov dx, 3948
+//   31   │ mov dx, -3948
+//   32   │ 
+//   33   │ ; Source address calculation
+//   34   │ mov al, [bx + si]
+//   35   │ mov bx, [bp + di]
+//   36   │ mov dx, [bp]
+//   37   │ 
+//   38   │ ; Source address calculation plus 8-bit displacement
+//   39   │ mov ah, [bx + si + 4]
+//   40   │ 
+//   41   │ ; Source address calculation plus 16-bit displacement
+//   42   │ mov al, [bx + si + 4999]
+//   43   │ 
+//   44   │ ; Dest address calculation
+//   45   │ mov [bx + di], cx
+//   46   │ mov [bp + si], cl
+//   47   │ mov [bp], ch
+
 
